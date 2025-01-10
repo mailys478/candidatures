39a7a126e351631c0a9a7b9563e134ffaa5c25c3
mailys <you@example.com>
2025-01-10 16:56:22 +0100

    candidaturesbis

--- depot.php
+++ depot.php
@@ -1,3 +1,13 @@
+<?php
+#var_dump($_SERVER['REQUEST_METHOD']);
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+var_dump($_FILES);
+move_uploaded_file($_FILES["candidatures"]["tmp_name"],"candidatures/".$_FILES["candidatures"]["name"]);
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,8 +18,8 @@
 <body>
   <h1>candidature</h1>
 
-  <form method="post">
-    <input type="file" name="candidature"/>
+  <form method="post" enctype="multipart/form-data">
+    <input type="file" name="candidatures"/>
     <button type="submit">Envoyer</button>
 </form>
 
