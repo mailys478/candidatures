4033aa0842b497d4f7c694b34866d01d1c641fd5
mailys <you@example.com>
2025-01-15 13:07:10 +0100

    test

--- candidatures.php
+++ candidatures.php
@@ -6,16 +6,25 @@
     <title>Document</title>
 </head>
 <body>
-<?php
-$dir = __DIR__."/candidatures";
-$dossier = new DirectoryIterator($dir);
-foreach ($dossier as $item) {
-if ($item->isFile()) { echo "Fichier : "; }
-if ($item->isDir()) { echo "Dossier : "; }
-echo $item->getFilename() . "<br>";
-}
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $dir = DIR."/candidatures";
+    $dossier = new DirectoryIterator($dir);
+    foreach ($dossier as $item) {
+    if (!$item->isDot() && $item->getFilename() !== '.gitkeep') {
+    if ($item->isFile()) {
+    echo "Fichier : ";
+    }
+    echo "<a href='candidatures/".$item->getFileName()."'>".$item->getFileName()."</a> <br>";
+    }
+    }
+    ?>
 </body>
 </html>
